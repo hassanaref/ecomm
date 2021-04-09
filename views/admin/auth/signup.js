3062166b5843a57bfc066a05aa9dcceaa4c5eb58
hassanaref <72562088+hassanaref@users.noreply.github.com>
2021-04-09 13:32:17 +0200

    dgf

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,14 +1,18 @@
 const layout = require('../layout');
+const { getError } = require('../../helpers');
 
-module.exports = ({ req }) => {
+module.exports = ({ req, errors }) => {
   return layout({
     content: `
     <div>
       Your id is: ${req.session.userId}
       <form method="POST">
         <input name="email" placeholder="email" />
+        ${getError(errors, 'email')}
         <input name="password" placeholder="password" />
+        ${getError(errors, 'password')}
         <input name="passwordConfirmation" placeholder="password confirmation" />
+        ${getError(errors, 'passwordConfirmation')}
         <button>Sign Up</button>
       </form>
     </div>
