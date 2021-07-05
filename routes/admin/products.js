4c83851d0f85988f4a06cddcf62e4c3ede184f00
hassanaref <72562088+hassanaref@users.noreply.github.com>
2021-07-05 22:32:03 +0200

    w

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 
-const { handleErrors,requireAuth } = require('./middlewares');
+const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
@@ -11,25 +11,22 @@ const { requireTitle, requirePrice } = require('./validators');
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products',requireAuth, async (req, res) => {
-
+router.get('/admin/products', requireAuth, async (req, res) => {
   const products = await productsRepo.getAll();
   res.send(productsIndexTemplate({ products }));
 });
 
 router.get('/admin/products/new', requireAuth, (req, res) => {
-
   res.send(productsNewTemplate({}));
 });
 
 router.post(
-  '/admin/products/new', 
+  '/admin/products/new',
   requireAuth,
   upload.single('image'),
   [requireTitle, requirePrice],
   handleErrors(productsNewTemplate),
   async (req, res) => {
-
     const image = req.file.buffer.toString('base64');
     const { title, price } = req.body;
     await productsRepo.create({ title, price, image });
@@ -38,17 +35,40 @@ router.post(
   }
 );
 
-router.get('/admin/products/:id/edit',requireAuth, async (req,res) => {
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
   const product = await productsRepo.getOne(req.params.id);
 
   if (!product) {
-    return res.send('product not found');
+    return res.send('Product not found');
   }
-  res.send(productsEditTemplate({product}));
+
+  res.send(productsEditTemplate({ product }));
 });
 
-router.post('admin/products/:id/edit', requireAuth, (req,res) => {
-  
-});
+router.post(
+  '/admin/products/:id/edit',
+  requireAuth,
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  handleErrors(productsEditTemplate, async req => {
+    const product = await productsRepo.getOne(req.params.id);
+    return { product };
+  }),
+  async (req, res) => {
+    const changes = req.body;
+
+    if (req.file) {
+      changes.image = req.file.buffer.toString('base64');
+    }
+
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      return res.send('Could not find item');
+    }
+
+    res.redirect('/admin/products');
+  }
+);
 
 module.exports = router;
--- views/admin/products/edit.js
+++ views/admin/products/edit.js
@@ -1,15 +1,17 @@
-const layout = require('../layout');
+const { getError } = require('../../helpers')
+const layout = require('../layout')
 
-module.exports = ({product}) => {
-    return layout({
-        content: `
-        <form method="POST">
-            <input name="title" value="${product.title}" />
-            <input name="price" value="${product.price}" />
-            <input name="image" type="file" />
-            <button>Submit</Button>
-        </form>
-
-        `
-    });
-};
+module.exports = ({ product, errors }) => {
+  return layout({
+    content: `
+      <form method="POST">
+        <input name="title" value="${product.title}" />
+        ${getError(errors, 'title')}
+        <input name="price" value="${product.price}" />
+        ${getError(errors, 'price')}
+        <input name="image" type="file" /> 
+        <button>Submit</button>
+      </form>
+    `
+  })
+}
