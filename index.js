8857a517644b02a9fc8b06475c5f5b4cf530c0b8
hassanaref <72562088+hassanaref@users.noreply.github.com>
2021-07-21 01:33:58 +0200

    .

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieSession = require('cookie-session')
-const authRouter = require('./routes/admin/auth')
-const adminProductsRouter = require('./routes/admin/products')
-const productsRouter = require('./routes/products')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
     keys: ['lkasld235j']
   })
-)
-app.use(authRouter)
-app.use(productsRouter)
-app.use(adminProductsRouter)
+);
+app.use(authRouter);
+app.use(productsRouter);
+app.use(adminProductsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
-  console.log('Listening')
-})
+  console.log('Listening');
+});
